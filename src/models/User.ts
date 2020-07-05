696eac9bef3a5aea9fbb00c3f1cd4211335dc9e7
khenryjoe <joseph.ketterer@gmail.com>
2020-07-05 16:42:48 +0100

    m

--- src/models/User.ts
+++ src/models/User.ts
@@ -21,14 +21,7 @@ export class User {
 
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
 
-  constructor(private data: UserProps) {}
 
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
  
 
 
