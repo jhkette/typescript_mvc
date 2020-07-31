329bc087489dbb6f657401e530e1095b36ab17df
khenryjoe <joseph.ketterer@gmail.com>
2020-07-31 11:51:03 +0100

    A

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
-import {User} from './models/User'
+import axios, {AxiosResponse} from 'axios';
 
-const user =  User.buildUser({name: "new record", age: 8})
-const user1 = User.buildUser({id: 1, name: 'newer name', age: 0})
-
-
-
-console.log(user.get('name'))
-
-user1.on('save', ()=> {
-    console.log(user1)
-})
-
-user1.save()
-// user.trigger('change')
-
-user.set({name: 'new name'})
+axios.get('http://localhost:3000/users')
+    .then((response: AxiosResponse) => {
+        console.log(response.data)
+    })
